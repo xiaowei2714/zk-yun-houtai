aa33ed63e9afe30c5aa6d70ba2d72cb943fe12ab
晓 <457294316@qq.com>
2025-05-25 17:32:21 +0800

    充值管理

--- src/views/recharge2/index.vue
+++ src/views/recharge2/index.vue
@@ -1,67 +1,111 @@
 <template>
     <div>
-<!--        <el-card class="!border-none mb-4" shadow="never">-->
-<!--            <el-form-->
-<!--                class="mb-[-16px]"-->
-<!--                :model="queryParams"-->
-<!--                inline-->
-<!--            >-->
-<!--                <el-form-item label="订单号" prop="order_no">-->
-<!--                    <el-input class="w-[280px]" v-model="queryParams.order_no" clearable placeholder="请输入订单号" />-->
-<!--                </el-form-item>-->
-<!--                <el-form-item>-->
-<!--                    <el-button type="primary" @click="resetPage">查询</el-button>-->
-<!--                    <el-button @click="resetParams">重置</el-button>-->
-<!--                </el-form-item>-->
-<!--            </el-form>-->
-<!--        </el-card>-->
+        <!--        <el-card class="!border-none mb-4" shadow="never">-->
+        <!--            <el-form-->
+        <!--                class="mb-[-16px]"-->
+        <!--                :model="queryParams"-->
+        <!--                inline-->
+        <!--            >-->
+        <!--                <el-form-item label="订单号" prop="order_no">-->
+        <!--                    <el-input class="w-[280px]" v-model="queryParams.order_no" clearable placeholder="请输入订单号" />-->
+        <!--                </el-form-item>-->
+        <!--                <el-form-item>-->
+        <!--                    <el-button type="primary" @click="resetPage">查询</el-button>-->
+        <!--                    <el-button @click="resetParams">重置</el-button>-->
+        <!--                </el-form-item>-->
+        <!--            </el-form>-->
+        <!--        </el-card>-->
         <div style="display: flex;margin-bottom: 20px;margin-top: 20px;justify-content: space-around">
-            <div style="width: 384px;height: 100px;background: #FFFFFF;box-shadow: 10px 20px 60px 1px rgba(0,0,0,0.05);border-radius: 20px 20px 20px 20px;">
+            <div
+                style="width: 384px;height: 100px;background: #FFFFFF;box-shadow: 10px 20px 60px 1px rgba(0,0,0,0.05);border-radius: 20px 20px 20px 20px;">
                 <div style="display: flex;justify-content: space-between;margin-left: 30px;margin-right: 30px">
                     <div style="margin-top: -20px;margin-left: 6px">
                         <img src="../../assets/icons/jinri.png" style="width: 60px;height: 60px" alt="">
                     </div>
-                    <div style="font-weight: bold;font-size: 24px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px;text-align: right;">0.00</div>
+                    <div
+                        style="font-weight: bold;font-size: 24px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px;text-align: right;">
+                        {{ querySum.today_sum }}
+                    </div>
                 </div>
-                <div style="display: flex;justify-content: space-between;margin-left: 30px;margin-right: 30px;margin-top: -10px">
-                    <div style="font-weight: 400;font-size: 13px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px">今日充值金额</div>
-                    <div style="font-weight: 400;font-size: 13px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px">0笔</div>
+                <div
+                    style="display: flex;justify-content: space-between;margin-left: 30px;margin-right: 30px;margin-top: -10px">
+                    <div
+                        style="font-weight: 400;font-size: 13px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px">
+                        今日充值金额
+                    </div>
+                    <div
+                        style="font-weight: 400;font-size: 13px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px">
+                        {{ querySum.today_count }}笔
+                    </div>
                 </div>
             </div>
-            <div style="width: 384px;height: 100px;background: #FFFFFF;box-shadow: 10px 20px 60px 1px rgba(0,0,0,0.05);border-radius: 20px 20px 20px 20px;">
+            <div
+                style="width: 384px;height: 100px;background: #FFFFFF;box-shadow: 10px 20px 60px 1px rgba(0,0,0,0.05);border-radius: 20px 20px 20px 20px;">
                 <div style="display: flex;justify-content: space-between;margin-left: 30px;margin-right: 30px">
                     <div style="margin-top: -20px;margin-left: 6px">
                         <img src="../../assets/icons/7day.png" style="width: 60px;height: 60px" alt="">
                     </div>
-                    <div style="font-weight: bold;font-size: 24px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px;text-align: right;">0.00</div>
+                    <div
+                        style="font-weight: bold;font-size: 24px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px;text-align: right;">
+                        {{ querySum.seven_days_sum }}
+                    </div>
                 </div>
-                <div style="display: flex;justify-content: space-between;margin-left: 30px;margin-right: 30px;margin-top: -10px">
-                    <div style="font-weight: 400;font-size: 13px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px">7天充值金额</div>
-                    <div style="font-weight: 400;font-size: 13px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px">0笔</div>
+                <div
+                    style="display: flex;justify-content: space-between;margin-left: 30px;margin-right: 30px;margin-top: -10px">
+                    <div
+                        style="font-weight: 400;font-size: 13px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px">
+                        7天充值金额
+                    </div>
+                    <div
+                        style="font-weight: 400;font-size: 13px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px">
+                        {{ querySum.seven_days_count }}笔
+                    </div>
                 </div>
             </div>
-            <div style="width: 384px;height: 100px;background: #FFFFFF;box-shadow: 10px 20px 60px 1px rgba(0,0,0,0.05);border-radius: 20px 20px 20px 20px;">
+            <div
+                style="width: 384px;height: 100px;background: #FFFFFF;box-shadow: 10px 20px 60px 1px rgba(0,0,0,0.05);border-radius: 20px 20px 20px 20px;">
                 <div style="display: flex;justify-content: space-between;margin-left: 30px;margin-right: 30px">
                     <div style="margin-top: -20px;margin-left: 6px">
                         <img src="../../assets/icons/30day.png" style="width: 60px;height: 60px" alt="">
                     </div>
-                    <div style="font-weight: bold;font-size: 24px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px;text-align: right;">0.00</div>
+                    <div
+                        style="font-weight: bold;font-size: 24px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px;text-align: right;">
+                        {{ querySum.month_sum }}
+                    </div>
                 </div>
-                <div style="display: flex;justify-content: space-between;margin-left: 30px;margin-right: 30px;margin-top: -10px">
-                    <div style="font-weight: 400;font-size: 13px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px">30天充值金额</div>
-                    <div style="font-weight: 400;font-size: 13px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px">0笔</div>
+                <div
+                    style="display: flex;justify-content: space-between;margin-left: 30px;margin-right: 30px;margin-top: -10px">
+                    <div
+                        style="font-weight: 400;font-size: 13px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px">
+                        30天充值金额
+                    </div>
+                    <div
+                        style="font-weight: 400;font-size: 13px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px">
+                        {{ querySum.month_count }}笔
+                    </div>
                 </div>
             </div>
-            <div style="width: 384px;height: 100px;background: #FFFFFF;box-shadow: 10px 20px 60px 1px rgba(0,0,0,0.05);border-radius: 20px 20px 20px 20px;">
+            <div
+                style="width: 384px;height: 100px;background: #FFFFFF;box-shadow: 10px 20px 60px 1px rgba(0,0,0,0.05);border-radius: 20px 20px 20px 20px;">
                 <div style="display: flex;justify-content: space-between;margin-left: 30px;margin-right: 30px">
                     <div style="margin-top: -20px;">
                         <img src="../../assets/icons/zj.png" style="width: 60px;height: 60px" alt="">
                     </div>
-                    <div style="font-weight: bold;font-size: 24px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px;text-align: right;">0.00</div>
+                    <div
+                        style="font-weight: bold;font-size: 24px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px;text-align: right;">
+                        {{ querySum.all_sum }}
+                    </div>
                 </div>
-                <div style="display: flex;justify-content: space-between;margin-left: 30px;margin-right: 30px;margin-top: -10px">
-                    <div style="font-weight: 400;font-size: 13px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px">总充值金额</div>
-                    <div style="font-weight: 400;font-size: 13px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px">0笔</div>
+                <div
+                    style="display: flex;justify-content: space-between;margin-left: 30px;margin-right: 30px;margin-top: -10px">
+                    <div
+                        style="font-weight: 400;font-size: 13px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px">
+                        总充值金额
+                    </div>
+                    <div
+                        style="font-weight: 400;font-size: 13px;color: #000000;font-style: normal;text-transform: none;margin-top: 15px">
+                        {{ querySum.all_count }}笔
+                    </div>
                 </div>
             </div>
 
@@ -69,13 +113,13 @@
         </div>
         <el-card class="!border-none" v-loading="pager.loading" shadow="never">
 
-<!--            <el-button-->
-<!--                v-perms="['recharge/delete']"-->
-<!--                :disabled="!selectData.length"-->
-<!--                @click="handleDelete(selectData)"-->
-<!--            >-->
-<!--                删除-->
-<!--            </el-button>-->
+            <!--            <el-button-->
+            <!--                v-perms="['recharge/delete']"-->
+            <!--                :disabled="!selectData.length"-->
+            <!--                @click="handleDelete(selectData)"-->
+            <!--            >-->
+            <!--                删除-->
+            <!--            </el-button>-->
             <el-form
                 class="mb-[-16px]"
                 :model="queryParams"
@@ -87,7 +131,7 @@
                 <el-form-item>
                     <el-button type="primary" @click="resetPage">查询</el-button>
                     <el-button @click="resetParams">重置</el-button>
-                    <el-button type="primary">
+                    <el-button @click="handleClear" type="primary">
                         <template #icon>
                             <icon name="el-icon-delete" />
                         </template>
@@ -98,17 +142,33 @@
             <div class="mt-4">
                 <el-table :data="pager.lists" @selection-change="handleSelectionChange">
                     <el-table-column type="selection" width="55" />
-                    <el-table-column label="用户ID" prop="user_id" show-overflow-tooltip />
+                    <el-table-column label="用户" prop="user_show" show-overflow-tooltip />
                     <el-table-column label="金额" prop="money" show-overflow-tooltip />
                     <el-table-column label="订单描述" prop="desc" show-overflow-tooltip />
-                    <el-table-column label="订单号" prop="order_no" show-overflow-tooltip />
-                    <el-table-column label="创建时间" prop="create_time" show-overflow-tooltip />
-                    <el-table-column label="付款时间" prop="pay_time" show-overflow-tooltip />
+                    <el-table-column label="订单号" prop="order_no" show-overflow-tooltip min-width="140" />
+                    <el-table-column label="创建时间" prop="create_time" show-overflow-tooltip min-width="120" />
+                    <el-table-column label="付款时间" prop="pay_time" show-overflow-tooltip min-width="120" />
                     <el-table-column label="回调稀哈" prop="hash" show-overflow-tooltip />
-                    <el-table-column label="订单状态" prop="status" show-overflow-tooltip />
+                    <el-table-column label="订单状态" prop="status" show-overflow-tooltip>
+                        <template #default="{ row }">
+                            <span v-if="row.status == 1">
+                                待付款
+                            </span>
+                            <span v-else-if="row.status == 2" style="color: #F8AC59">
+                                已付款
+                            </span>
+                            <span v-else-if="row.status == 3" style="color: #1AB394;">
+                                已完成
+                            </span>
+                            <span v-else-if="row.status == 4" style="color: #E24555">
+                                已失败
+                            </span>
+                            <span v-else></span>
+                        </template>
+                    </el-table-column>
                     <el-table-column label="操作" width="120" fixed="right">
                         <template #default="{ row }">
-                             <el-button
+                            <el-button
                                 v-perms="['recharge/edit']"
                                 type="primary"
                                 link
@@ -139,10 +199,11 @@
 <script lang="ts" setup name="rechargeLists">
 import { usePaging } from '@/hooks/usePaging'
 import { useDictData } from '@/hooks/useDictOptions'
-import { apiRechargeLists, apiRechargeDelete } from '@/api/recharge'
+import { apiRechargeLists, apiSum, apiRechargeDelete, apiClearOrder } from '@/api/recharge'
 import { timeFormat } from '@/utils/util'
 import feedback from '@/utils/feedback'
 import EditPopup from './edit.vue'
+import { apiSetRecharging } from '@/api/consume_recharge'
 
 const editRef = shallowRef<InstanceType<typeof EditPopup>>()
 // 是否显示编辑框
@@ -157,6 +218,9 @@ const queryParams = reactive({
 // 选中数据
 const selectData = ref<any[]>([])
 
+// 汇总数据
+const querySum = ref<any[]>([])
+
 // 表格选择后回调事件
 const handleSelectionChange = (val: any[]) => {
     selectData.value = val.map(({ id }) => id)
@@ -170,6 +234,11 @@ const { pager, getLists, resetParams, resetPage } = usePaging({
     fetchFun: apiRechargeLists,
     params: queryParams
 })
+
+// 汇总
+const getSum = async () => {
+    querySum.value = await apiSum()
+}
 
 // 添加
 const handleAdd = async () => {
@@ -193,6 +262,14 @@ const handleDelete = async (id: number | any[]) => {
     getLists()
 }
 
+// 清空未支付订单
+const handleClear = async (id: number | any[]) => {
+    await feedback.confirm('确定要清理20分钟前未支付订单？')
+    await apiClearOrder()
+    getLists()
+}
+
 getLists()
+getSum()
 </script>
 
